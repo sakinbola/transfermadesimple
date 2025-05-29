4bec53cda161d23cf5d3478063bf880794b01b62
Seyi <sakinbolaa@gmail.com>
2025-05-29 18:16:53 -0400

    header done going to add google signin?

--- src/pages/sections/Header.jsx
+++ src/pages/sections/Header.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
-
+import "./header.css"
 
 function Header() {
 
   return (
-    <div>Header</div>
+    <div className="header">
+
+      <a className = "logo" href=""> 
+        <img src="" alt="" /> Logo 
+      </a>
+
+
+      <a href=""> Send </a>
+      <a href=""> Collect </a>
+      <a href=""> Contact Us</a>
+
+      <a href=""> Log In </a>
+      <a href=""> Sign Up </a>
+    </div>
   )
 }
 
 export default Header
+
+// add hamburger menu for mobile view or short screen 
