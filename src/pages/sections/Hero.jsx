81894d18b8b5026edc08852c48640e8cae7988c3
Seyi <sakinbolaa@gmail.com>
2025-05-29 16:11:56 -0400

    added hero

--- src/pages/sections/Hero.jsx
+++ src/pages/sections/Hero.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-
+import "./hero.css"
 
 function Hero() {
 
   return (
-    <div>Hero Section</div>
+    <div className="hero"> 
+      <h1 className="animate__animated animate__fadeInDown"> Transfer Made Simple</h1>
+      <p className="animate__animated animate__fadeInUp animate__delay-1s"> 
+        Port your videos , ideas and creations between devices with ease
+      </p>
+    
+    </div>
   )
 }
 
